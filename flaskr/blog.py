fabd6bc9d5ac399e0ad173f69630d36281c3104f
Brian Karabinchak <karabinchak.brian@gmail.com>
2019-04-10 21:11:57 -0400

    Added like button and all functionality

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -11,11 +11,28 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
+
+    if g.user:
+        userId = g.user['id']
+    else:
+        userId = -1
+    print("user: "+str(userId))
+
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, SUM(l.postId=p.id) as likes, SUM(l.userId=?) as personallyLiked'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' LEFT JOIN likedPosts l ON l.postId = p.id'
+        ' GROUP BY p.id'
+        ' ORDER BY created DESC',
+        (userId,)
     ).fetchall()
+    print("posts: " + str(posts))
+    countOfPosts = db.execute('SELECT COUNT(*) FROM post').fetchone()[0]
+    print("post count: " + str(countOfPosts))
+    if posts:
+        print("posts"+str(posts[0]))
+        for stuff in posts[0]:
+            print(str(stuff))
     return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -94,3 +111,41 @@ def delete(id):
 	db.execute('DELETE FROM post WHERE id = ?', (id,))
 	db.commit()
 	return redirect(url_for('blog.index'))
+
+@bp.route('/<int:id>/like', methods=('POST',))
+@login_required
+def like(id):
+    """
+    Arguments:
+        id: id of the post to be liked
+    Returns:
+        Nothing as of now, will figure this out later
+    """
+    db = get_db()
+    if g.user:
+        userId = g.user['id']
+    else:
+        userId = -1
+
+    userAlreadyLikedPost = db.execute('SELECT COUNT(*) FROM likedPosts WHERE postId = ? AND userId = ?', (id, userId)).fetchone()[0] > 0
+    
+    if userAlreadyLikedPost:
+        #Then delete the record, the user unlikes the post
+        db.execute(
+            'DELETE FROM likedPosts'
+            ' WHERE postId = ? AND userId = ?',
+            (id, userId)
+        )
+        db.commit()
+        print("Unliked post")
+    else:
+        # Like this post
+        db.execute(
+            'INSERT INTO likedPosts (postId, userId)'
+            ' VALUES (?, ?)',
+            (id, g.user['id'])
+        )
+        db.commit()
+        print("Liked post")
+    
+    return redirect(url_for('blog.index'))
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS likedPosts;
 
 CREATE TABLE user (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,3 +16,11 @@ CREATE TABLE post (
 	body TEXT NPT NULL,
 	FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+CREATE TABLE likedPosts (
+	postId INTEGER NOT NULL,
+	userId INTEGER NOT NULL
+);
+
+INSERT INTO likedPosts (postId, userId)
+VALUES (0,0);
